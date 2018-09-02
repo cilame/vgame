0b3c3928be630721526fb634d4b9addf6209cde7
cilame <2473495041@qq.com>
2018-09-02 17:11:27 +0800

    update

--- vgame.py
+++ vgame.py
@@ -143,7 +143,7 @@ class artist:
             self.currrent = theater.theater_name # 第一次注册的舞台将默认作为入口舞台
 
 
-    # test func. 随机切换到非当前场景的其他场景的方法，只有一个场景的话不会切换，用于测试
+    # test func. 随机切换到非当前场景的其他场景的方法，只有一个场景的话不会切换，该函数仅用于测试
     def _random_change(self):
         v = list(self.theaters)
         v.remove(self.currrent)
@@ -165,27 +165,32 @@ class theater:
     def __init__(self,
                  bg_filename,       # 背景图片（不确定开发：动态背景？例如白天黑夜效果？其实，白天黑夜可以通过加一层半透明黑的actor实现。）
                  theater_name,      # 场景名字，用于定位、调整、切换场景使用
-                 screen_pos=(0,0),  # 后期扩展，扩大缩小地图时候可能需要的参数（可能不仅限于此）
                  blocks=None,       # 后期扩展，用于配置该场景是否需要切分细块，可指定两个数字参数的tuple或list，作为横纵切分数量
-                 singles=[(30,30),(40,40),(60,60),(80,80)], # 单元大小，blocks被设定时，该参数会被使用，默认使用第一个，扩展地图缩放
-                 single_line=[(255,255,255),1],               # 单元格分割线的颜色和粗细
+                 singles=[(30,30),(40,40),(60,60),(80,80)],   # 单元大小，blocks被设定时，该参数会被使用，默认使用第一个，扩展地图缩放，以后考虑扩展更顺滑的缩放
+                 single_line=[(255,255,255,150),1],           # 单元格分割线的颜色和粗细，这个默认值暂且是为了开发方便
                  music=None         # 后期扩展，音乐
                  ):
         self.theater_name   = theater_name
-        self.screen_pos     = screen_pos
+        self.screen_pos     = (0,0)
         self.group          = pygame.sprite.Group()
         self.background     = None
         self.artist         = None
 
+        # 用于缩放功能的参数
         self.blocks         = blocks
         self.single         = singles[0]
         self.singles        = singles
         self.singles_image  = {}
         self.single_color   = single_line[0] if single_line else None
         self.single_thick   = single_line[1] if single_line else None
+        self.toggle         = True
 
         # 创建每个场景都需要一个默认的背景，图片加载失败就会用默认的某种颜色填充
         self._add_background(bg_filename)
+
+        # 用于坐标定位的参数
+        self.point2map      = None
+        self.map2point      = None
 
         if blocks:
             self._mk_blocks_map() # 对背景切块,blocks切块参数传递的地方
@@ -198,7 +203,6 @@ class theater:
             self.group.add(actor)
             actor.theater = self
 
-
     # 创建切块的地图信息，slg，2drpg...绝大多数非动作类游戏都有这样的需求。
     def _mk_blocks_map(self):
 
@@ -207,55 +211,54 @@ class theater:
         singlew, singleh = self.single
         width,   height  = self.background.image.get_size()
         new_w,   new_h   = singlew*ncols, singleh*nraws
-        cur_single = int(width/ncols),int(height/nraws)
+        cur_single       = int(width/ncols),int(height/nraws)
+
+
+        # 节约资源的做法，同时也能保证不会因为尺寸变化丢失背景图片精度
+        if self.single in self.singles_image:
+            self.background.image = self.singles_image[self.single]
+        else:
+            self.singles_image[self.single] = pygame.transform.scale(self.background.image,(new_w,new_h))
 
         if self.single != cur_single:
-            # 节约资源的做法，同时也能保证不会因为尺寸变化丢失背景图片精度
-            if self.single in self.singles_image:
-                self.background.image = self.singles_image[self.single]
-            else:
-                self.singles_image[self.single] = pygame.transform.scale(self.background.image,(new_w,new_h))
-                self.background.image = pygame.transform.scale(self.background.image,(new_w,new_h))
+            self.background.image = self.singles_image[self.single]
 
-        # 画网格线条，便于查看显示，这里没有与背景一体化是考虑可能会有动态关闭显示方块线的功能
-        # 当然也有其他扩展的方法能更节省资源，不过这不是重点以后再考虑
+        # 当然也有其他扩展的方法能更节省资源。目前没有动态开关线条的功能，后续再考虑开发。
         if self.single_color:
             color,thick = self.single_color,self.single_thick
             for i in range(1,ncols):
                 pygame.draw.line(self.background.image,color,(i*singlew,0),(i*singlew,new_h),thick)
             for i in range(1,nraws):
                 pygame.draw.line(self.background.image,color,(0,i*singleh),(new_w,i*singleh),thick)
+            self.single_color = None # 解决由于地图缩放，导致的线条重绘制使得显示线条变粗
 
-        print(self.single,(width, height),(ncols, nraws))
 
     # 每个场景的创建必须要一张图片作为背景。
     def _add_background(self, bg_filename):
         
-        # 背景也是actor实现
+        # 背景也是通过 actor类实例化实现
         act = actor(bg_filename)
         def _default_theater(screen, ticks):
-            # 键盘 + - 控制缩放
-            self._change_size() 
-
-            sw,sh = screen.get_size()
-            bw,bh = self.background.image.get_size()
-            px = (sw-bw)/2 if bw<=sw else 0
-            py = (sh-bh)/2 if bh<=sh else 0
-
-            # 通过修改rect来修改放置的中心点
-            act.rect[0] = px
-            act.rect[1] = py
-
-
-
-
-            # 目前的工作就需要在这里处理完中心缩放以及后续添加一个找地图坐标点的函数
-            #=======================================================================
-
             
+            # 键盘 + - 控制缩放
+            if self._change_size():
+                self.toggle = True
+
+            # 控制缩放时的坐标。用toggle的好处为：在初始化时能够执行一次，之后需要缩放成功才会再度执行修改配置的代码
+            if self.toggle:
+                sw,sh = screen.get_size()
+                bw,bh = self.background.image.get_size()
+                kw,kh = sw/2 - self.screen_pos[0], sh/2 - self.screen_pos[1]
+                tw,th = int(kw - bw/2), int(kh - bh/2)
+                px = (sw-bw)/2 if bw<=sw else tw
+                py = (sh-bh)/2 if bh<=sh else th
+                act.rect[0] = px # 通过修改rect来修改放置的中心点
+                act.rect[1] = py
+                self.screen_pos = (px, py) # 存储在类里面，后续应用于与其他函数交互
+                self.toggle = False
 
         act.update = _default_theater
-        
+
         if act.image:
             self.group.add(act)
             self.background = act
@@ -272,15 +275,19 @@ class theater:
             if self.single != ret:
                 self.single = ret
                 self._mk_blocks_map()
+                return True
 
-        # 这里需要设计两种放大缩小的方式，一种是blocks式的缩放，另一种只是单纯的缩放。
+        # 这里进行了一次统一，之后都统一使用blocks式的缩放。因为这样实现可以更方便。
+        # 也就是说，如果你需要缩放功能，请设置blocks参数，另外，将方块黏着的需求交给开发者会更能扩展实现功能。
         # 这里暂时想不到更巧妙的按键设计，pygame.event.get() 属于消耗型事件，所以不能随便堆加使用。
         if pygame.key.get_pressed()[K_KP_PLUS]:
             if self.blocks:
-                _change_blocks('+'); time.sleep(.1)
+                v = _change_blocks('+'); time.sleep(.1)
+                return v
         if pygame.key.get_pressed()[K_KP_MINUS]:
             if self.blocks:
-                _change_blocks('-'); time.sleep(.1)
+                v = _change_blocks('-'); time.sleep(.1)
+                return v
         
 
 class actor(pygame.sprite.Sprite):
@@ -297,7 +304,8 @@ class actor(pygame.sprite.Sprite):
         # 加载图片倘若名字符合动图处理规则加载函数内就会修改self.active，用动图处理方式
         self.image      = self.load_img(img)
         self.rect       = self.image.get_rect() if self.image else None
-        self.theater    = None
+        self.theater    = None # 将该对象注册进 theater之后会自动绑定相应的 theater。
+        self.viscous    = False
 
     def load_img(self,img):
         if img is None: return None
@@ -336,19 +344,24 @@ class actor(pygame.sprite.Sprite):
 
         return image
 
-    def _time_update(self, ticks):
+    # 控制速率的函数，使任意帧率情况下都保持相同速度的函数
+    def _time_update(self,ticks):
         if ticks - self.cur_tick > self.rate:
             self.cur_tick = ticks
             return True
 
     def update(self,screen,ticks):
-        if self.active and self._time_update(ticks):
+
+        rate = self._time_update(ticks) # 由于 actor天生需要非常多的速度控制，所以 rate复用性很高
+        
+        if self.active and rate:
             self.image = self.src_image.subsurface(next(self.rects))
 
         # 通过以下方法可以实现修改图片大小，不过由于修改大小这部应该会消耗资源，
         # 每次加载都要修改一次大小，所以之后考虑优化的方式，让速度变得更快一些
         # 或者可以考虑放在 self.load_img函数当中，地图切块时需要考虑单元块的适配
         # 将 actor注册进 theater可以获取 theater里面设定的单位大小，后续可能需要扩展多格单位
+        # 另外，这里的配置也不太科学。如果有blocks必然会修改尺寸，写太死了，后续再改。目前用于测试比较方便。
         if self.theater.blocks:
             self.image = pygame.transform.scale(self.image,self.theater.single) 
 
@@ -362,6 +375,10 @@ class actor(pygame.sprite.Sprite):
         # 测试删除对象本身，后期删除
         if pygame.key.get_pressed()[K_s]:# 键盘s键删除自身单位
             self.kill()
+
+    # 黏性方块，让该对象具备黏性，传入参数可以是地图坐标，或者是一个鼠标坐标
+    def rect_viscous(self,point):
+        pass
 
 
 class initer:
@@ -416,16 +433,17 @@ class initer:
 
 
 if __name__ == "__main__":
-    bg1 = 'sushipla1te.jpg'
+    bg1 = 'sushiplate.jpg'
     bg2 = 'sea.jpg'
     cur = 'sprite_100x100_32.png'
 
-    s = initer()
+    s = initer(50) # 第一个参数为全局帧率，默认60
     v1 = theater(bg1,'123') # theater必须要指定两个元素，背景图片地址，舞台名字（舞台切换用）
     actor1 = actor(cur)
     v1.regist(actor1)
     
 
+    # 指定blocks之后会按照列行数量对图片切分，然后以每块大小默认为（30，30）进行缩放显示，支持动态缩放
     v2 = theater(bg2,'sea',blocks=(16,12))
     actor2 = actor(cur)
     v2.regist(actor2)
