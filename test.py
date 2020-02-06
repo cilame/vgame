6bc4620648ab99570a0a360a528bd4e3853a73a0
cilame <opaquism@hotmail.com>
2020-02-06 13:41:38 +0800

    add

--- test.py
+++ test.py
@@ -12,16 +12,16 @@ if __name__ == "__main__":
     main = Initer(60) # 第一个参数为全局秒帧率，默认60
 
     # 场景一
-    main_theater = Theater(bg1, '123') # theater 必须要指定两个元素，1背景图片资源地址，2舞台名字
+    theater_0 = Theater(bg1, '123') # theater 必须要指定两个元素，1背景图片资源地址，2舞台名字
     actor1 = Actor(cur, showsize=(40,40), in_control=True) # in_control 负责接收控制指令，目前控制指令只会打印相关的操作内容
-    main_theater.regist(actor1) # 动作者注册进场景才能使用
-    main.regist(main_theater) # 场景需要注册进初始对象才能使用
+    theater_0.regist(actor1) # 动作者注册进场景才能使用
+    main.regist(theater_0) # 场景需要注册进初始对象才能使用
 
     # 场景二
-    theater_0 = Theater(bg2, 'sea') # theater 必须要指定两个元素，1背景图片资源地址，2舞台名字
+    theater_1 = Theater(bg2, 'sea') # theater 必须要指定两个元素，1背景图片资源地址，2舞台名字
     actor2 = Actor(cur, showsize=(80,80), in_control=True)
-    theater_0.regist(actor2)
-    main.regist(theater_0)
+    theater_1.regist(actor2)
+    main.regist(theater_1)
 
 
     main.change_theater('sea') # 可以通过舞台名字进行场景的切换
--- vgame/events.py
+++ vgame/events.py
@@ -15,11 +15,17 @@ class Events:
     可以通过 in_control 参数进行配置是否需要使用控制处理
     状态转化 # 战斗计算 # 血条显示之类 # 属性配置之类 # 子弹发射 # 我个人想到什么就扩展什么，各种功能...
     '''
-    def __init__(self, event_rate=0, in_control=False):
+    def __init__(self, event_rate=60, in_control=False):
         # 
         self.actor    = None
         self.rate     = event_rate  # 
         self.cur_tick = 0           # 总延时帧率部分的参数
+
+        # 因为之后设计的所有按键操作都将会“放弃”通过 pygame.event.get() 获取事件的方式来获取控制
+        # 因为 pygame.event.get() 多次在不同地方使用的时候，会出现各种问题（测试结论）。
+        # 这无疑是对单击事件是非常灾难性的。灵敏度丢失不可接受， 所以这里一律使用 pygame.key.get_pressed() 来实现功能，
+        # 这里通过延迟来实现按键执行的速率，避免任意需要延迟的操作漂移。
+        # 这个地方为通用延时，和鼠标延时（鼠标延时类似于一种等待）还是有一些不太一样的。
 
         # 用于鼠标操作的参数（一般只有在鼠标按下时候会更新，所以不用担心资源浪费问题）
         self.mouse_id     = None # in (0,1,2) 0代表左键，2代表右键，1代表中键
@@ -27,21 +33,20 @@ class Events:
         self.mouse_status = 0    # 按键状态（0无按，1按下，2松开瞬间）
         self.mouse_toggle = True
         self.mouse_tick   = 0
-        self.mouse_delay  = 300  # 鼠标按下多久不动后转变成地图拖拽模式 # 关于延迟的动态配置后期再做，这里先硬编码
+        self.mouse_delay  = 300  # 鼠标按下多久不动后转变成地图拖拽模式
         self.cross_time   = False
         self.cross_status = 0    # 判断状态（0判断，1单击，2框选，3地图拖拽）
         self.limit_len    = 3.   # 一个阈值，与鼠标按下松开时，两点坐标点距离有关
 
         # 用于键盘方向操作的参数
-        self.direction_key_tick   = 0    # 后期发现只用 self._delay 函数，如果混合其他操作可能会出现灵敏丢失的情况
-        self.direction_key_delay  = 100  # 所以最后还是把键盘的操作延时也单独出来了 # 关于延迟的动态配置后期再做，这里先硬编码
-        self.un_oblique           = 0    # 如果没有斜角操作的话，处理斜角的操作滞粘操作参数
+        self.direction_key_tick   = 0  # 后期发现只用 self._delay 函数，如果混合其他操作可能会出现灵敏丢失的情况
+        self.direction_key_delay  = 10 # 所以最后还是把键盘的操作延时也单独出来了
+        self.un_oblique           = 0  # 如果没有斜角操作的话，处理斜角的操作滞粘操作参数
 
         # 用于键盘控制操作的参数
         self.control_key_tick     = 0
-        self.control_key_delay    = 100
-        self.control_keys = [pygame.K_j,pygame.K_k]
-
+        self.control_key_delay    = 10
+        self.control_keys = [pygame.K_j, pygame.K_k]
 
         # 用于实现栈效果式的更新函数
         self.update_stack = []
@@ -58,42 +63,20 @@ class Events:
         if self.update_stack:
             self.update_stack[-1](ticks)
 
-
-
-
-    def _delay(self,ticks,rate):
-        # 核心延迟函数，保证接收命令的帧率，防止操作灵敏度过高
-        # 不过其他的操作不能直接用这个来实现延迟，因为直接用 self._delay 控制不好调整不同操作的接收频率。
-        
-        # 因为之后设计的所有按键操作都将会“放弃”通过 pygame.event.get() 获取事件的方式来获取控制
-        # 因为 pygame.event.get() 多次在不同地方使用的时候，会出现各种问题（测试结论）。
-        # 这无疑是对单击事件是非常灾难性的。灵敏度丢失不可接受， 所以这里一律使用 pygame.key.get_pressed() 来实现功能，
-        # 这里通过延迟来实现按键执行的速率，避免任意需要延迟的操作漂移。
-        # 这个地方为通用延时，和鼠标延时（鼠标延时类似于一种等待）还是有一些不太一样的。
-        if ticks - self.cur_tick > rate:
-            self.cur_tick = ticks
-            return True
-
     def update_select_cursor(self,ticks):
         # 光标类主要是用于可以被控制所操作的对象，该类对象可以通过下面的函数来对操作进行挂钩
         # 后续可以开发部分功能用于该处与外部通信，让外部自定义的函数能够使用下面挂钩到的接口
-
-        if self._delay(ticks,self.rate):
-            direc_info = self.general_direction_key(ticks,wasd=True) # 处理通常方向键
-            morse_info = self.general_mouse_model_a(ticks)           # 处理通常鼠标键
-            
-            # 简单打印操作接口返回的内容，后续会进行开发处理
-            if morse_info:
-                print(self.actor, morse_info)
-            if direc_info:
-                print(self.actor, direc_info)
-
-
-
-
-
-
-
+        morse_info = self.general_mouse(ticks,model='a')             # 处理通常鼠标键，有两种模式，请看函数内的注释描述
+        direc_info = self.general_direction_key(ticks,direct='wasd') # 处理通常方向键
+        cntro_info = self.general_control_key(ticks)                 # 处理通常ab键的接收
+        
+        # 简单打印操作接口返回的内容，后续会进行开发处理
+        if morse_info:
+            print(self.actor, morse_info)
+        if direc_info:
+            print(self.actor, direc_info)
+        if cntro_info:
+            print(self.actor, cntro_info)
 
     #===============#
     #               #
@@ -104,59 +87,80 @@ class Events:
     #==============#
     # 一般鼠标操作 #
     #==============#
-    def general_mouse_model_a(self,ticks):
-        # 这里的点击模式包含了类似地图抓取拖动的功能
-        # 返回的参数为 按键id（0左键，1中键[不包含滚轮]，2右键），功能（1单击，2直接框选，3按住后再拖动），起点坐标，终点坐标
-        # 当然如果没有 “按住后再拖动” 的功能需求，直接判断返回结果的第一个参数是否在 2，3 里面即可。
+    # 鼠标的操作有两个函数，分别表示两种处理方式
+    # model=='a': 这种类型仅含单击和框选两种模式
+    # model=='b': 这种类型包含单击、框选和长按三种模式
+    # 该两个函数的返回参数均如下所示，仅在功能上是否返回3这点区别
+    # 返回的参数为 按键id（0左键，1中键[不包含滚轮]，2右键），功能（1单击，2直接框选，3按住后再拖动），起点坐标，终点坐标
+    # 非常建议使用 a 类，这种处理非常顺滑，
+    def general_mouse(self,ticks,model='a'):
+        # 这里的点击模式仅仅包含单击和框选两种处理方式
         rem = self._mouse_pressed() # rem 究竟是什么，详细请看 _mouse_pressed 函数注释。
-        if rem and rem[0] in (0, 2):
-            if self.cross_status == 0:
-                if rem[1] == 1:
-                    # 随时检查长度和时间
-                    if self._mouse_delay(ticks,self.mouse_delay):
-                        if self.cross_time:
-                            if rem[4] < self.limit_len:
-                                self.cross_status = 3
-                            if rem[4] >= self.limit_len:
-                                self.cross_status = 2
-                        self.cross_time = True
-                if rem[1] == 2:
-                    if rem[4] >= self.limit_len:
-                        self.cross_status = 2
+        def model_a(rem):
+            if rem and rem[0] in (0, 2):
+                if self.cross_status == 0:
+                    if rem[1] == 1:
+                        if rem[4] >= self.limit_len:
+                            self.cross_status = 2
+                    if rem[1] == 2:
+                        if rem[4] >= self.limit_len:
+                            self.cross_status = 2
+                        else:
+                            self.cross_status = 1
+                # 1单击状态 # 就是鼠标单击
+                if self.cross_status == 1:
+                    self.cross_status = 0
+                    return rem[0], self.cross_status, rem[2:4]
+                # 2框选状态 # 鼠标快速框选某个区域
+                if self.cross_status == 2:
+                    if rem[1] == 2:
+                        self.cross_status = 0
                     else:
-                        self.cross_status = 1
+                        return rem[0], self.cross_status, rem[2:4]
+        def model_b(rem):
+            if rem and rem[0] in (0, 2):
+                if self.cross_status == 0:
+                    if rem[1] == 1:
+                        # 随时检查长度和时间
+                        if self._mouse_delay(ticks):
+                            if self.cross_time:
+                                if rem[4] < self.limit_len:
+                                    self.cross_status = 3
+                                if rem[4] >= self.limit_len:
+                                    self.cross_status = 2
+                            self.cross_time = True
+                    if rem[1] == 2:
+                        if rem[4] >= self.limit_len:
+                            self.cross_status = 2
+                        else:
+                            self.cross_status = 1
+                            self.cross_time = False
+                # 1单击状态 # 就是鼠标单击
+                if self.cross_status == 1:
+                    self.cross_status = 0
+                    return rem[0], self.cross_status, rem[2:4]
+                # 2框选状态 # 鼠标快速框选某个区域
+                if self.cross_status == 2:
+                    if rem[1] == 2:
+                        self.cross_status = 0
                         self.cross_time = False
-            # 1单击状态 # 就是鼠标单击
-            if self.cross_status == 1:
-                self.cross_status = 0
-                return rem[0], 1, rem[2:4]
-            # 2框选状态 # 鼠标快速框选某个区域
-            if self.cross_status == 2:
-                if rem[1] == 2:
-                    self.cross_status = 0
-                    self.cross_time = False
-                else:
-                    return rem[0], 2, rem[2:4]
-            # 3地图状态 # 鼠标长按一点，然后拖动
-            if self.cross_status == 3:
-                if rem[1] == 2:
-                    self.cross_status = 0
-                    self.cross_time = False
-                else:
-                    return rem[0], 3, rem[2:4]
-    def _mouse_delay(self,ticks,rate):
+                    else:
+                        return rem[0], self.cross_status, rem[2:4]
+                # 3地图状态 # 鼠标长按一点，然后拖动
+                if self.cross_status == 3:
+                    if rem[1] == 2:
+                        self.cross_status = 0
+                        self.cross_time = False
+                    else:
+                        return rem[0], self.cross_status, rem[2:4]
+        if model == 'a': return model_a(rem)
+        if model == 'b': return model_b(rem)
+    def _mouse_delay(self,ticks):
         # 鼠标的延迟在这里更类似于一种等待，实现鼠标长按功能
-        if ticks - self.mouse_tick > rate:
+        if ticks - self.mouse_tick > self.mouse_delay:
             self.mouse_tick = ticks
             return True
     def _mouse_pressed(self):
-        # 鼠标的话需要考虑几个状态
-        # 1 左键单击（持续按，不超过一个很微小的时间，然后松开）
-        # 2 左键持续（持续按，超过一个很微小的时间）
-        # 3 右键同理
-        # 4 右键同理
-        # 5 复选模式（可能需要返回一个界面区域的rect交给后续处理，返回值也要考虑
-        # ...
         # 【返回参数】：
         # 按键id（0左键，1中键[不包含滚轮]，2右键），按键状态（0无按，1按下，2松开瞬间），起点坐标，终点坐标，两坐标之间的长
         mouse = pygame.mouse.get_pressed()
@@ -188,10 +192,10 @@ class Events:
     #================#
     # 一般方向键操作 #
     #================#
-    def general_direction_key(self,ticks,wasd=False,oblique=True):
-        rek = self._direction_key_pressed(wasd) # 获取当前按键（或组合键）的方向，以小键盘八方向数字为准
+    def general_direction_key(self,ticks,direct='wasd',oblique=True):
+        rek = self._direction_key_pressed(direct) # 获取当前按键（或组合键）的方向，以小键盘八方向数字为准
         # 需要先判断rek再判断延迟才能防止出现灵敏丢失的情况
-        if rek and self._direction_key_delay(ticks,self.direction_key_delay):
+        if rek and self._direction_key_delay(ticks):
             target = []
             if rek == 4: target.append(LEFT)
             if rek == 6: target.append(RIGHT)
@@ -199,8 +203,8 @@ class Events:
             if rek == 2: target.append(DOWN)
             if oblique:
                 # 允许有斜角的处理方法（默认允许斜角处理）# 这样处理不会出现对顶的可能性，注意
-                if rek == 7: target.append(LEFT), target.append(UP)
-                if rek == 1: target.append(LEFT), target.append(DOWN)
+                if rek == 7: target.append(LEFT),  target.append(UP)
+                if rek == 1: target.append(LEFT),  target.append(DOWN)
                 if rek == 9: target.append(RIGHT), target.append(UP)
                 if rek == 3: target.append(RIGHT), target.append(DOWN)
             else:
@@ -220,19 +224,19 @@ class Events:
                     if self.un_oblique == 2: target.append(RIGHT)
                     if self.un_oblique == 6: target.append(DOWN)
             return target
-    def _direction_key_delay(self,ticks,rate):
-        if ticks - self.direction_key_tick > rate:
+    def _direction_key_delay(self,ticks):
+        if ticks - self.direction_key_tick > self.direction_key_delay:
             self.direction_key_tick = ticks
             return True
-    def _direction_key_pressed(self,wasd=False):
+    def _direction_key_pressed(self,direct='wasd'):
         key = pygame.key.get_pressed()
         ret = 0
-        if wasd:
-            a = key[pygame.K_UP]    or key[pygame.K_w]
-            b = key[pygame.K_DOWN]  or key[pygame.K_s]
-            c = key[pygame.K_LEFT]  or key[pygame.K_a]
-            d = key[pygame.K_RIGHT] or key[pygame.K_d]
-        else:
+        if direct == 'wasd':
+            a = key[pygame.K_w]
+            b = key[pygame.K_s]
+            c = key[pygame.K_a]
+            d = key[pygame.K_d]
+        if direct == 'uldr':
             a = key[pygame.K_UP]
             b = key[pygame.K_DOWN]
             c = key[pygame.K_LEFT]
@@ -277,7 +281,13 @@ class Events:
         # 类似于一般手柄上面的AB键位的功能，简单的提供一些接口
         # 让某些键位注册后返回一些对于编写者来说更方便查调用的数据接口
         # 类似注册了键盘 jk 键位为 ab（手柄），按j、k键时候返回为a、b，之类的。ab会更好进行功能辨识
-        pass
+        rck = self._control_key_pressed() # 获取当前按键（或组合键）的方向，以小键盘八方向数字为准
+        if rck and self._control_key_delay(ticks):
+            return rck
+    def _control_key_delay(self,ticks):
+        if ticks - self.control_key_tick > self.control_key_delay:
+            self.control_key_tick = ticks
+            return True
     def _control_key_pressed(self):
         # 一些普通的键盘非方向键的按键返回，这里需要考虑的是一般游戏ab键的单按和连发的区分和识别
         # 所以可能需要和鼠标那样的方式去实现功能。方向键和ab键和鼠标都是需要各自的延迟参数(必须)
