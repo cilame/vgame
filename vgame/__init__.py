2d5f65342b94423509709b02a4b1405e6c879233
cilame <opaquism@hotmail.com>
2020-10-08 02:22:24 +0800

    add

--- vgame/__init__.py
+++ vgame/__init__.py
@@ -184,6 +184,6 @@ def change_theater(name):
 
 
 __author__ = 'cilame'
-__version__ = '0.0.9'
+__version__ = '0.1.0'
 __email__ = 'opaquism@hotmail.com'
 __github__ = 'https://github.com/cilame/vgame'
