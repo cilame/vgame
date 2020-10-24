6abb7fdb743b099dab124d4225ed088ed1998e89
cilame <opaquism@hotmail.com>
2020-10-24 18:01:03 +0800

    add

--- vgame/image.py
+++ vgame/image.py
@@ -183,14 +183,6 @@ class Image:
         else:
             return image.copy()
 
-        def rot_center(image, angle):
-            orig_rect = image.get_rect()
-            rot_image = pygame.transform.rotate(image, angle)
-            rot_rect = orig_rect.copy()
-            rot_rect.center = rot_image.get_rect().center
-            rot_image = rot_image.subsurface(rot_rect).copy()
-            return rot_image
-
     def _mk_mask(self):
         if self.masksize:
             if not getattr(self, 'mskdefault', None):
